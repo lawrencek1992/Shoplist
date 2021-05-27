0e5270743d98fa4bc062720f065a7044a99d0ecb
Kelly <lawrencek1992@gmail.com>
2021-05-27 17:00:22 -0600

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const List = () => {
     return (
-        <ul className="list">
+        <ul className="List">
             <li>
                 eggs
             </li>
