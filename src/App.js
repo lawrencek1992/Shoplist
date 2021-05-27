762d4758539044a25ed02432273c6d460400dc31
Kelly <lawrencek1992@gmail.com>
2021-05-27 15:17:31 -0600

    Add AddButton component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import List from './components/List';
+import AddButton from './components/AddButton';
 
 function App() {
   return (
     <div className="App">
       <List />
+      <AddButton />
     </div>
   );
 }
---
+++ src/components/AddButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddButton = () => {
+    return (
+        <div className="AddButton">
+            Here is the AddButton!!!
+        </div>
+    );
+}
+
+export default AddButton;
