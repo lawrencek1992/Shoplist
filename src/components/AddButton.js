e8abb36da04667b217fb941c5129e390fa464317
Kelly <lawrencek1992@gmail.com>
2021-05-27 17:00:01 -0600

    Add notes to AddButton.js

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+// write a function to open the modal when you click the + button
+
 const AddButton = () => {
     return (
-        <div className="AddButton">
+        // Add an onClick property to run open the modal
+        <div className="AddButton btn-danger bg-gradient">
             +
         </div>
     );
