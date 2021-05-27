a348ee9e5fd58c58a60dddfdbb9aff56860ef7e6
Kelly <lawrencek1992@gmail.com>
2021-05-27 15:13:22 -0600

    Add List.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import List from './components/List';
+
 function App() {
   return (
     <div className="App">
-
+      <List />
     </div>
   );
 }
---
+++ src/components/List.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const List = () => {
+    return (
+        <ul className="list">
+            <li>
+                eggs
+            </li>
+            <li>
+                milk
+            </li>
+            <li>
+                cream
+            </li>
+            <li>
+                figs
+            </li>
+      </ul>
+    )
+}
+
+export default List;
