2317daa1672e36a8623e710268e357f3bb1dac2d
Kelly <lawrencek1992@gmail.com>
2021-05-28 00:01:03 -0600

    Add state and functions to show and hide modal

--- src/App.js
+++ src/App.js
@@ -3,15 +3,39 @@ import './App.css';
 import './custom.scss';
 
 import List from './components/List';
-import AddButton from './components/AddButton';
+import AddItemForm from './components/AddItemForm';
 
 function App() {
   const [showModal, setShowModal] = useState(false);
+  // const [listItems, setListItems] = useState([]);
+
+  const handleButtonClick = () => {
+    setShowModal(true);
+  }
+
+  const handleHide = () => {
+    setShowModal(false);
+  }
+
+  // function handleClose () {
+  //   setShowModal(false);
+  // }
+
+  // const handleShow = () => {
+  //   setShow(true);
+  // }
+
+  // useEffect(() => {
+  //   handleShow();
+  // }, [show])
 
   return (
     <div className="App">
       <List />
-      <AddButton />
+      <AddItemForm showModal={showModal} handleHide={handleHide}/>
+      <div className="AddButton btn-danger bg-gradient" onClick={handleButtonClick}>
+            +
+        </div>
     </div>
   );
 }
