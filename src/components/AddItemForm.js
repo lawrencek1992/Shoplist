9ef99d90eb01d69e50b0306943f1f92e3758fe89
Kelly <lawrencek1992@gmail.com>
2021-05-28 01:19:17 -0600

    Style modal

--- src/components/AddItemForm.js
+++ src/components/AddItemForm.js
@@ -1,23 +1,23 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import { Modal, Button } from 'react-bootstrap';
 
 const AddItemForm = ({showModal, handleHide}) => {
 
     return (
         <Modal show={showModal} onHide={handleHide} centered>
-            <Modal.Header closeButton>
+            <Modal.Header id="modal">
                 <Modal.Title>
                     Add Item
                 </Modal.Title>
+                <Button type="close" className="btn btn-close" id="btn-close" ariaLabel="Close" onClick={handleHide} />
             </Modal.Header>
-            <Modal.Body>
+            <Modal.Body id="modal">
                 <p>
                     Here is where you will put the input form!
                 </p>
             </Modal.Body>
-            <Modal.Footer>
-                <Button className="btn btn-default" type="submit" onClick={handleHide}>
+            <Modal.Footer id="modal">
+                <Button className="btn btn-success" type="submit" onClick={handleHide}>
                     Submit
                 </Button>
                 <Button className="btn btn-secondary" type="cancel" onClick={handleHide}>
