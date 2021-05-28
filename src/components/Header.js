b7459017fd0b73022b486ea3379b49ea8bb8fc39
Kelly <lawrencek1992@gmail.com>
2021-05-28 12:53:45 -0600

    Add Brand and Buttons to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
+import { Navbar, Button, Nav } from 'react-bootstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBag } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     return (
-        <Navbar variant="dark" fixed="top">
-            What shall I put in the navbar?
+        <Navbar 
+            bg="dark" 
+            variant="dark" 
+            className="justify-content-between"
+            fixed="top">
+            <Navbar.Brand href="/">
+                <FontAwesomeIcon 
+                    icon={faShoppingBag} id="shopping-icon" 
+                    style={{ "margin-left": "10px", "margin-right": "5px",}}
+                />
+                Shoplist
+            </Navbar.Brand>
+            <Nav>
+                <Button 
+                    className="btn-danger text-right"
+                    style={{ "margin-right": "10px", }}
+                >
+                    Sign Up
+                </Button>
+                <Button 
+                    variant="outline-success"
+                    style={{ "margin-right": "10px", }}
+                >
+                    Login
+                </Button>
+            </Nav>
         </Navbar>
     );
 }
