12c0aa450f8eadecca809a30e71c8e3ce1873896
Kelly <lawrencek1992@gmail.com>
2021-05-28 23:20:07 -0600

    Create login form in Login.js component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,42 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { useHistory } from "react-router-dom";
+import { Container, Form, Button } from 'react-bootstrap';
+import firebase from '../firebase';
 
 const Login = () => {
+    const history = useHistory();
+
+    // login function
+
     return (
-        <Container classname="bg-dark" fluid>
-            <p>This is the login page!</p>
+        <Container className="Login" fluid>
+            <Container>
+                <Form id="login-form">
+                    <Form.Group>
+                        <Form.Label>
+                            Email
+                        </Form.Label>
+                        <Form.Control type="email" />
+                    </Form.Group>
+                    <br />
+                    <Form.Group>
+                        <Form.Label>
+                            Password
+                        </Form.Label>
+                        <Form.Control type="text" />
+                    </Form.Group>
+                    <br />
+                    <Button id="login-button" className="btn btn-success">
+                        Login
+                    </Button>
+                    <Button 
+                        className="btn btn-secondary"
+                        onClick={() => history.push("/")}
+                    >
+                        Cancel
+                    </Button>
+                </Form>
+            </Container>
         </Container>
     );
 }
