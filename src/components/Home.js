d016e47d4b7749924b3235377c3118c7e662a72c
Kelly <lawrencek1992@gmail.com>
2021-05-28 22:52:29 -0600

    Add state to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState } from 'react';
+import { useStorageState } from 'react-storage-hooks';
 import List from './List';
 import AddItemForm from './AddItemForm';
 import { Container } from 'react-bootstrap';
 
-const Home = ({ listItems, deleteItem, showModal, handleHide, addNewItem, handleButtonClick }) => {
+const Home = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [listItems, setListItems] = useStorageState(localStorage, `state-list-items`, []);
+
+  const handleButtonClick = () => {
+    setShowModal(true);
+  }
+
+  const handleHide = () => {
+    setShowModal(false);
+  }
+
+  const addNewItem = (item) => {
+    const updatedList = listItems.concat(item);
+    setListItems(updatedList);
+  }
+
+  const deleteItem = (item) => {
+    const index = listItems.indexOf(item);
+    const updatedList = [...listItems];
+    updatedList.splice(index, 1);
+    setListItems(updatedList);
+  }
     return (
         <Container className="Home" fluid>
                 <List 
