77c9af8c4427b1eda36f00f608413e398081a3b7
Kelly <lawrencek1992@gmail.com>
2021-05-30 19:15:03 -0600

    Add TransitionGroup elements

--- src/components/List.js
+++ src/components/List.js
@@ -1,39 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { 
+    TransitionGroup,
+    CSSTransition
+} from 'react-transition-group'
 import { ListGroup, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 const List = ({listItems, deleteItem}) => {
-    const [isFadingOut, setIsFadingOut] = useState(false);
-
-    const fadeOut = (cb) => {
-        setIsFadingOut(true);
-    };
 
     const handleDelete = (item) => {
         deleteItem(item);
-        setIsFadingOut(false);
     }
 
     return (
         <ListGroup className="list-group">
-            {listItems && listItems
-                .map((item) => (
-                    <ListGroup.Item id={item.category} key={item.key} className={isFadingOut ? 'item-fadeout bg-gradient' : 'item bg-gradient'}> 
-                        <Row>
-                            <Col className="col-10">
-                                {item.name}
-                            </Col>
-                            <Col>
-                                <FontAwesomeIcon icon={faTrashAlt} className="col-2 icon" onClick={() => fadeOut( 
-                                    setTimeout(() => handleDelete(item), 300))}
-                                />
-                            </Col>
-                        </Row>   
-                    </ListGroup.Item>
-                ))
+            <TransitionGroup>
+                {listItems && listItems
+                    .map((item) => (
+                        <CSSTransition
+                            key={item.key}
+                            timeout={500}
+                            classNames="item-fade"
+                        >
+                            <ListGroup.Item id={item.category} key={item.key}className="bg-gradient">
+                                <Row>
+                                    <Col className="col-10">
+                                    {item.name}
+                                    </Col>
+                                    <Col>
+                                        <FontAwesomeIcon icon={faTrashAlt} className="col-2 icon" onClick={()=> {
+                                            handleDelete(item)
+                                        }}
+                                    />
+                                    </Col>
+                                </Row>   
+                            </ListGroup.Item>
+                        </CSSTransition>
+                    ))
                 // sort function here
-            }
+                }
+            </TransitionGroup>
         </ListGroup>
     )
 }
