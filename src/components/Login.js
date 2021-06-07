aeb6351ab40513c4b40ce6fbce2b97c3daca283c
Kelly <lawrencek1992@gmail.com>
2021-06-07 01:44:31 -0600

    Add form validation to Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,26 @@
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import { Container, Form, Button } from 'react-bootstrap';
-// import UserContext from '../context/UserContext';
 // import firebase from '../firebase.js';
 
 const Login = (onLogin) => {
     const [loginCreds, setLoginCreds] = useState({});
+    const [validated, setValidated] = useState(false);
 
     const history = useHistory();
 
-    const handleLogin = () => {
+    const handleLogin = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+            event.preventDefault();
+            setValidated(true);
+        }
+
+
+
         // Write a function to handle authentication in firebase and then save that to userContext;
         setLoginCreds({});
     }
@@ -17,37 +28,49 @@ const Login = (onLogin) => {
     return (
         <Container className="Login" fluid>
             <Container>
-                <Form id="login-form">
-                    <Form.Group>
+                <Form 
+                    id="login-form"
+                    noValidate 
+                    validated={validated}
+                    onSubmit={handleLogin}>
+                    <Form.Group controlId="validationCustom05">
                         <Form.Label>
                             Email
                         </Form.Label>
-                        <Form.Control 
+                        <Form.Control
+                            required
                             type="email" 
                             onChange={(event) => setLoginCreds({
                                 email: event.target.value,
                                 password: loginCreds.password,
                             })}
                         />
+                        <Form.Control.Feedback type="invalid">
+                                Email required
+                        </Form.Control.Feedback>
                     </Form.Group>
                     <br />
-                    <Form.Group>
+                    <Form.Group controlId="validationCustom06">
                         <Form.Label>
                             Password
                         </Form.Label>
-                        <Form.Control 
+                        <Form.Control
+                            required
                             type="text" 
                             onChange={(event) => setLoginCreds({
                                 email: loginCreds.email,
                                 password: loginCreds.password,
                             })}
                         />
+                        <Form.Control.Feedback type="invalid">
+                                Password required
+                        </Form.Control.Feedback>
                     </Form.Group>
                     <br />
                     <Button 
                         id="login-button" 
                         className="btn btn-success"
-                        onClick={() => handleLogin()}
+                        onClick={(event) => handleLogin(event)}
                     >
                         Login
                     </Button>
