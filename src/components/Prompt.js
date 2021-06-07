e3c4e96441b681b2752beab4855ead00c1035baa
Kelly <lawrencek1992@gmail.com>
2021-06-07 03:31:47 -0600

    Create Prompt.js

---
+++ src/components/Prompt.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Prompt = () => {
+    return (
+        <Container className="prompt">
+            Sign up or log in to your existing account in order to add items to your own shopping list!
+        </Container>
+    )
+}
+
+export default Prompt;
