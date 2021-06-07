17963fd1f95f8eae7a33e32842692a7c21585a4c
Kelly <lawrencek1992@gmail.com>
2021-06-07 04:10:24 -0600

    Add basic Prompt.js content/styles

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { ReactComponent as UpArrow } from '../media/curved-up-arrow-.svg';
+
 
 const Prompt = () => {
     return (
+			<>
         <Container className="prompt">
             Sign up or log in to your existing account in order to add items to your own shopping list!
         </Container>
+				<UpArrow className="up-arrow" />
+			</>
     )
 }
 
