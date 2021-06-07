033b4799213484275b083f5e29a3d9b2c37e9d74
Kelly <lawrencek1992@gmail.com>
2021-06-07 03:32:20 -0600

    Update AccountSuccess.js

--- src/components/AccountSuccess.js
+++ src/components/AccountSuccess.js
@@ -4,45 +4,45 @@ import { Modal, Button } from 'react-bootstrap';
 
 const AccountSuccess = ({ showAccountSuccess, handleHideAccountSuccess, user }) => { 
 
-    const history = useHistory();
+  const history = useHistory();
 
-    return (
-        <Modal
-            show={showAccountSuccess}
-            onHide={() => {
-                handleHideAccountSuccess();
-            }}
-            centered
-        >
-            <Modal.Header id="success-modal"> 
-                <Button 
-                    type="close"
-                    className="btn btn-close" 
-                    id="btn-close" 
-                    ariaLabel="Close"
-                    onClick={() => { 
-                        handleHideAccountSuccess();
-                    }} 
-                />
-            </Modal.Header>
-            <Modal.Body id="success-modal">
-                Thank you for creating a Shoplist account, <strong>{user.name}! </strong>
-                Please log in to add items to your shopping list.
-            </Modal.Body>
-            <Modal.Footer id="success-modal">
-                    <Button
-                        className="btn btn-secondary"
-                        id="login-redirect"
-                        onClick={() => {
-                            handleHideAccountSuccess();
-                            history.push('/login');
-                        }}
-                    >
-                        Login
-                    </Button>
-            </Modal.Footer>
-        </Modal>
-    );
+	return (
+		<Modal
+			show={showAccountSuccess}
+			onHide={() => {
+					handleHideAccountSuccess();
+			}}
+			centered
+	>
+			<Modal.Header id="success-modal"> 
+				<Button 
+					type="close"
+					className="btn btn-close" 
+					id="btn-close" 
+					ariaLabel="Close"
+					onClick={() => { 
+						handleHideAccountSuccess();
+					}} 
+				/>
+			</Modal.Header>
+			<Modal.Body id="success-modal">
+				Thank you for creating a Shoplist account, <strong>{user.name}! </strong>
+				Please log in to add items to your shopping list.
+			</Modal.Body>
+			<Modal.Footer id="success-modal">
+				<Button
+					className="btn btn-secondary"
+					id="login-redirect"
+					onClick={() => {
+						handleHideAccountSuccess();
+						history.push('/login');
+					}}
+				>
+					Login
+				</Button>
+			</Modal.Footer>
+		</Modal>
+	);
 }
 
 export default AccountSuccess;
