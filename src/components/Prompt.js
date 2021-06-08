bd1997e2d1e910c6fce04c0f7c2deed82c1be1c7
Kelly <lawrencek1992@gmail.com>
2021-06-07 20:12:44 -0600

    Add fadeInAnimation to list-group and prompt message

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import { CSSTransition } from 'react-transition-group';
 import { ReactComponent as UpArrow } from '../media/curved-up-arrow-.svg';
 
 
 const Prompt = () => {
     return (
 			<>
-        <Container className="prompt">
-            Sign up or log in to your existing account in order to add items to your own shopping list!
-        </Container>
-				<UpArrow className="up-arrow" />
+					<Container className="prompt">
+							Sign up or log in to your existing account in order to add items to your own shopping list!
+					</Container>
+				<CSSTransition>
+					<UpArrow className="up-arrow" />
+				</CSSTransition>
 			</>
     )
 }
