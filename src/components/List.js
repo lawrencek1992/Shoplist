3f3f180a349d42943d6dc9fb83d693497970a857
Kelly <lawrencek1992@gmail.com>
2021-06-11 19:11:24 -0600

    Display a prompt if List.js is empty

--- src/components/List.js
+++ src/components/List.js
@@ -3,7 +3,7 @@ import {
     TransitionGroup,
     CSSTransition
 } from 'react-transition-group'
-import { ListGroup, Row, Col } from 'react-bootstrap';
+import { Container, ListGroup, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
@@ -14,33 +14,44 @@ const List = ({listItems, deleteItem}) => {
     }
 
     return (
-        <ListGroup className="list-group">
-            <TransitionGroup>
-                {listItems && listItems
-                    .map((item) => (
-                        <CSSTransition
-                            key={item.key}
-                            timeout={500}
-                            classNames="item-fade"
-                        >
-                            <ListGroup.Item id={item.category} key={item.key}className="bg-gradient">
-                                <Row>
-                                    <Col className="col-10">
-                                    {item.name}
-                                    </Col>
-                                    <Col>
-                                        <FontAwesomeIcon icon={faTrashAlt} className="col-2 icon" onClick={()=> {
-                                            handleDelete(item)
-                                        }}
-                                    />
-                                    </Col>
-                                </Row>   
-                            </ListGroup.Item>
-                        </CSSTransition>
-                    ))
-                }
-            </TransitionGroup>
-        </ListGroup>
+        <>
+        { (listItems.length == [])
+            ? (
+                <Container className="empty-list" fluid>
+                    <p>Use the add button to create a shopping list!</p>
+                </Container>
+            )
+            : (
+                <ListGroup className="list-group">
+                    <TransitionGroup>
+                    {listItems
+                        .map((item) => (
+                            <CSSTransition
+                                key={item.key}
+                                timeout={500}
+                                classNames="item-fade"
+                            >
+                                <ListGroup.Item id={item.category} key={item.key}className="bg-gradient">
+                                    <Row>
+                                        <Col className="col-10">
+                                        {item.name}
+                                        </Col>
+                                        <Col>
+                                            <FontAwesomeIcon icon={faTrashAlt} className="col-2 icon" onClick={()=> {
+                                                handleDelete(item)
+                                            }}
+                                        />
+                                        </Col>
+                                    </Row>   
+                                </ListGroup.Item>
+                            </CSSTransition>
+                        ))
+                    }
+                </TransitionGroup>
+            </ListGroup>
+            )
+        }
+        </>
     )
 }
 
